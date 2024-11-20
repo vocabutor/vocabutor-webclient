597ffaa7be97bae643d01eda9b0d8e793a803f59
Nima Ajdari <me@nimaajdari.com>
2024-11-20 15:18:05 +0100

    basic router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
-import Login from './components/pages/Login'
 
-function App() {
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
+const App: React.FC = () => {
   return (
-      <Login />
-  )
-}
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> |
+        <Link to="/login">Login</Link>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Login from './components/pages/Login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
