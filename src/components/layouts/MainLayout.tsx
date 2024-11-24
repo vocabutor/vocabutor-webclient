9fae55aa422d399bcb1247e68911931bc37aa9e7
Nima Ajdari <me@nimaajdari.com>
2024-11-24 19:55:43 +0100

    some minor styling improvements

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import './MainLayout.css'
 
 const MainLayout: React.FC = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> |
-        <Link to="/auth/login">Login</Link>
-      </nav>
-      <hr />
+    <div className="layout">
+      {/* Header */}
+      <header className="header">
+        <Link to="/">
+            <div className="logo">Vocabutor</div>
+        </Link>
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search decks or cards…"
+        />
+        <nav className="nav-menu">
+          <Link to="/" className="nav-link active">
+            Home
+          </Link>
+          <Link to="/decks" className="nav-link">
+            My Decks
+          </Link>
+          <Link to="/profile" className="nav-link">
+            Profile
+          </Link>
+        </nav>
+      </header>
+
       <Outlet />
+
+      <footer className="footer">
+        <Link className="footer-link" to="/about">About Us</Link>
+        <Link className="footer-link" to="/support">Support</Link>
+        <Link className="footer-link" to="/social">Social Links</Link>
+      </footer>
     </div>
   );
 };
